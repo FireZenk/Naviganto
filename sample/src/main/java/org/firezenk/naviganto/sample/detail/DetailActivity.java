7498cb06131aacc1223298420b5c3e8c99a01b90
Jorge Garrido <firezenk@gmail.com>
2017-03-07 11:58:51 +0100

    Prepare new samples for the new back actions

--- sample/src/main/java/org/firezenk/naviganto/sample/detail/DetailActivity.java
+++ sample/src/main/java/org/firezenk/naviganto/sample/detail/DetailActivity.java
@@ -12,6 +12,7 @@ import org.firezenk.naviganto.annotations.RoutableActivity;
 import org.firezenk.naviganto.library.Naviganto;
 import org.firezenk.naviganto.library.Route;
 import org.firezenk.naviganto.sample.R;
+import org.firezenk.naviganto.sample.home.HomeActivity;
 import org.firezenk.naviganto.sample.home.HomeRoute;
 import org.firezenk.naviganto.sample.product.ProductViewRoute;
 import org.firezenk.naviganto.sample.profile.ProfileRoute;
@@ -26,6 +27,8 @@ import java.util.Random;
 @RoutableActivity(params = {String.class, String.class, Integer.class}, requestCode = -1)
 public class DetailActivity extends AppCompatActivity {
 
+    private int times = 0;
+
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detail_activity);
@@ -33,15 +36,28 @@ public class DetailActivity extends AppCompatActivity {
         final ViewGroup placeholder = (ViewGroup) findViewById(R.id.placeholder);
         final TextView title = (TextView) findViewById(R.id.title);
         final Button nextButton = (Button) findViewById(R.id.next);
+        final Button prevButton = (Button) findViewById(R.id.prev);
         final Button actButton = (Button) findViewById(R.id.act);
+        final Button prevActButton = (Button) findViewById(R.id.prev_act);
+        final Button timesButton = (Button) findViewById(R.id.times);
 
         title.setText(getIntent().getStringExtra("string0") + getIntent().getStringExtra("string1") + getIntent().getIntExtra("int2", 0));
 
+        updateTimesButton(timesButton, times);
+
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
+                updateTimesButton(timesButton, times++);
                 final Object[] params = new Object[1];
                 params[0] = new Random().nextDouble();
                 Naviganto.get().routeTo(DetailActivity.this, new Route<>(ProductViewRoute.class, params, placeholder));
+            }
+        });
+
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                updateTimesButton(timesButton, times--);
+                onBackPressed();
             }
         });
 
@@ -51,11 +67,21 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
+        prevActButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View view) {
+                Naviganto.get().backTo(DetailActivity.this, new Route<>(HomeRoute.class, new Bundle()));
+            }
+        });
+
         Naviganto.get().routeTo(this, new Route<>(ProfileRoute.class, new Bundle(), placeholder));
     }
 
     @Override public void onBackPressed() {
         if (!Naviganto.get().back(this))
             super.onBackPressed();
+    }
+
+    private void updateTimesButton(Button timesButton, int times) {
+        timesButton.setText("go back " + times + " times");
     }
 }
